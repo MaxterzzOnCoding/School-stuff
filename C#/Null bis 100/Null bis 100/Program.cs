afff79888f5dcac69af633319649b615b3b3a72b
Ismet Marek <116670021+DerIsmet@users.noreply.github.com>
2022-11-08 10:24:00 +0100

    Add files via upload

---
+++ C#/Null bis 100/Null bis 100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Null_bis_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+               if (i < 100)
+                {
+                    Console.Write("" + i + ", ");
+                }
+               else
+                {
+                    Console.Write("" + i + "");
+                }
+            }
+        }
+    }
+}
