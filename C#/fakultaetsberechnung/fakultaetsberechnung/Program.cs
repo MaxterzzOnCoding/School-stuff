e0ca952e63ffbe9be7d2af850732558ea6d1aca3
Ismet Marek <116670021+DerIsmet@users.noreply.github.com>
2022-11-22 10:57:09 +0100

    Add files via upload

---
+++ C#/fakultaetsberechnung/fakultaetsberechnung/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace fakultaetsberechnung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eingabe;
+            int faku = 1;
+
+            Console.WriteLine("Fakultätsberechnung");
+            Console.WriteLine("");
+            Console.Write("Natürliche Zahl eingeben: ");
+            eingabe = Convert.ToInt32(Console.ReadLine());
+
+            for (int fak = 1; fak <= eingabe; fak++)
+            {
+                faku = fak * faku;
+            }
+            Console.WriteLine(""+eingabe+"! = "+faku);
+        }
+    }
+}
