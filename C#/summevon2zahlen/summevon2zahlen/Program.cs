643980eee02dfe0929f1adb9ad5da30909f977dd
Ismet Marek <116670021+DerIsmet@users.noreply.github.com>
2022-11-22 09:56:47 +0100

    Add files via upload

---
+++ C#/summevon2zahlen/summevon2zahlen/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace summevon2zahlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1;
+            int zahl2;
+            int summe = 0;
+            string jnstring;
+            char jn;
+
+          
+
+            do
+            {
+                Console.WriteLine("------------------------------");
+                Console.Write("Bitte Zahl 1 eingeben: ");
+                zahl1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Bitte Zahl 2 eingeben: ");
+                zahl2 = Convert.ToInt32(Console.ReadLine());
+                summe = zahl1 + zahl2;
+                Console.WriteLine("Die Summe lautet: " + summe + "");
+                Console.WriteLine("------------------------------");
+                Console.Write("Wollen Sie das Programm nochmals starten? (J/N)    ");
+                jnstring = Console.ReadLine();
+                jn = Convert.ToChar(jnstring);
+            } while (jn == 'j' || jn == 'J') ;
+
+
+
+
+        }
+    }
+}
---
+++ C#/wurfel/wurfel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace wurfel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random zufgen = new Random();
+            int zahl;
+            zahl = 0;
+            int zahl1 = 0;
+   ;
+
+            Console.WriteLine("Würfelspiel");
+            Console.WriteLine("============");
+
+            do
+            {
+             zahl = zufgen.Next(1, 7);
+                Console.WriteLine("gewürfelte Zahl:"+zahl);
+                zahl1 = zahl1 + zahl ;
+            
+            } while (zahl < 6);
+            zahl1 = zahl1 - 6;
+            Console.WriteLine("Summe:"+zahl1);
+            Console.ReadKey();
+        }
+    }
+}
